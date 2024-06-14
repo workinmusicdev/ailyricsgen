47e70a5995972db0a0efbedc68a2b6e838bd5e94
princeGedeon <guedjegedeon03@gmail.com>
2024-06-14 12:12:00 +0100

    save

--- app.py
+++ app.py
@@ -563,9 +563,4 @@ async def job_generate_lyrics_multi_from_theme(
     }
 
 
-# Lancer l'application
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
 #rq worker task_queue
